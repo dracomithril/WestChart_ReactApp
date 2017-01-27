aaab8ad5ea5a39cc7238014c1d4e0f9cfdb0e15d
Draco Mithril <dracomithril@gmail.com>
2017-01-27 12:34:54 +0100

    v0.1.5 nice logging button user greeting changed icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
  * Created by Gryzli on 25.01.2017.
  */
 import React from 'react';
-import {Grid, Row, Col, Panel, Image} from 'react-bootstrap';
+import {Image, Well} from 'react-bootstrap';
 import logo from './starter.png';
 import {FacebookLogin} from 'react-facebook-login-component';
 import 'bootstrap-social';
@@ -30,35 +30,27 @@ export default class Header extends React.Component {
         const user = this.state.user;
         let showIt = this.state.user.accessToken === undefined;
         return (<div className="App-header">
-            <Grid>
-                <Row className="show-grid">
-                    <Col xs={8} md={6}><img src={logo} className="App-logo" alt="logo"/></Col>
-                    <Col xs={6} md={3}>
-                        <Panel header="User info" bsStyle={showIt?"warning":"primary"}>
-                            {!showIt && <div>
-                                <Image src={user.picture.data.url} circle/>
-                                <span
-                                    style={{color: 'black'}}>{`Hi, ${user.first_name} it's nice to see you again.`}</span>
-                            </div>}
-                            <a className="btn btn-block btn-social btn-twitter">
-                                <span className="fa fa-twitter"/>
-                                Sign in with Twitter
-                            </a>
-                            {showIt && <FacebookLogin
-                                socialId={process.env.NODE_ENV === 'production' ? "1173483302721639" : "1173486879387948"}
-                                language="pl_PL"
-                                scope="public_profile,email"
-                                responseHandler={this.responseFacebook.bind(this)}
-                                xfbml={true}
-                                fields="id,email,name,first_name,picture"
-                                version="v2.8"
-                                class="btn-block btn-social btn-sm btn-facebook"
-                                buttonText="Login With Facebook"/>}
-                        </Panel>
-                    </Col>
-                </Row>
-            </Grid>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <Well bsClass={showIt ? "well" : "logged"}>
 
+                {!showIt && <div>
+                    <Image src={user.picture.data.url} circle/>
+                    <span>{`Hi, ${user.first_name}`}</span><br/>
+                    <span>{`it's nice to see you again.`}</span>
+                </div>}
+                {showIt && <FacebookLogin
+                    socialId={process.env.NODE_ENV === 'production' ? "1173483302721639" : "1173486879387948"}
+                    language="pl_PL"
+                    scope="public_profile,email"
+                    responseHandler={this.responseFacebook.bind(this)}
+                    xfbml={true}
+                    fields="id,email,name,first_name,picture"
+                    version="v2.8"
+                    class="btn-login"
+                    buttonText={<a className="btn btn-block btn-social btn-facebook">
+                        <span className="fa fa-facebook"/> Sign in with Facebook
+                    </a>}/>}
+            </Well>
         </div>);
     }
 }
