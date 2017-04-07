b6d33958b8486fb3def426eac421cd696cee2329
Draco Mithril <dracomithril@gmail.com>
2017-04-07 19:02:11 +0200

    auto load list

--- src/components/PickYourDate.jsx
+++ src/components/PickYourDate.jsx
@@ -6,6 +6,19 @@ import {Accordion, Button, Checkbox, Label, Panel} from "react-bootstrap";
 import DatePicker from "react-datepicker";
 let utils = require('./../utils');
 export default class PickYourDate extends React.Component {
+    componentWillUnmount() {
+        console.log('component ChartPresenter unmounted');
+    }
+
+    componentDidMount() {
+        console.log('component ChartPresenter did mount');
+        const {store} = this.context;
+        const {user} = store.getState();
+        if (user.isGroupAdmin) {
+            this.updateChart()
+        }
+    }
+
     updateChart() {
         const {store} = this.context;
         const {user, enable_until, start_date, show_last} = store.getState();
@@ -25,9 +38,10 @@ export default class PickYourDate extends React.Component {
         };
         utils.get_chart_from_server(query_params, store);
     }
+
     render() {
         const {store} = this.context;
-        const {user, enable_until, last_update,start_date,show_last,since,until} = store.getState();
+        const {user, enable_until, last_update, start_date, show_last, since, until} = store.getState();
         const footer = (<small
             id="updateDate">{' Last update: ' + new Date(last_update).toLocaleString('pl-PL')}</small>);
         return (<Accordion>
@@ -39,8 +53,8 @@ export default class PickYourDate extends React.Component {
                            min={0} max={62}
                            value={show_last}
                            step={1}
-                           onChange={(e)=>{
-                               store.dispatch({type:'UPDATE_SHOW_LAST',days:Number(e.target.value)})
+                           onChange={(e) => {
+                               store.dispatch({type: 'UPDATE_SHOW_LAST', days: Number(e.target.value)})
                            }}/>{' days'}
                 </label>
                 <Checkbox checked={enable_until} name="enable_until"
@@ -51,7 +65,7 @@ export default class PickYourDate extends React.Component {
                     <DatePicker
                         selected={start_date}
                         dateFormat="DD/MM/YYYY"
-                        onChange={date=>store.dispatch({type:'UPDATE_START_TIME',date})}
+                        onChange={date => store.dispatch({type: 'UPDATE_START_TIME', date})}
                         disabled={!enable_until}/>
                 </Checkbox>
                 {(since !== '' && until !== '') &&
