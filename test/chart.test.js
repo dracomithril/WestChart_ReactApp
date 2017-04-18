d261ecff3a477d783e1052cae2aead56868f7d84
Grezel Michal-XKTR67 <XKTR67@motorolasolutions.com>
2017-04-18 12:50:59 +0200

    fix tests

--- test/chart.test.js
+++ test/chart.test.js
@@ -34,14 +34,32 @@ describe('[chart]', function () {
         let groupId = '1707149242852457';
         const body1 = {
             statusCode: 200,
-            body: JSON.stringify(test_body)
+            body: test_body
+        };
+        let options = {
+            "uri": "/v2.8/1707149242852457/feed",
+            "baseUrl": "https://graph.facebook.com",
+            "qs": {
+                "fields": "story,from,link,caption,icon,created_time,source,name,type,message,attachments,full_picture,updated_time,likes.limit(1).summary(true),reactions.limit(1).summary(true),comments.limit(50).summary(true){message,from}",
+                "limit": 100,
+                "access_token": "",
+                "since": "2017-01-31T23:00:00.000Z",
+                "until": "2017-03-03T23:00:00.000Z"
+            },
+            "port": 443,
+            "path": "/v2.8/1707149242852457/feed",
+            "method": "GET",
+            "timeout": 9000,
+            "simple": true,
+            "json": true,
+            "resolveWithFullResponse": false
         };
         const body2 = {
             statusCode: 200,
-            body: JSON.stringify(test_body2)
+            body: test_body2
         };
-        requestMock.getAsync.withArgs(sinon.match.object).returns(Promise.resolve(body1));//callsArgWith(1, undefined, {statusCode: 200}, JSON.stringify(test_body));
-        requestMock.getAsync.withArgs(test_body.paging.next).returns(Promise.resolve(body2));//callsArgWith(1, undefined, {statusCode: 200}, JSON.stringify(test_body2));
+        requestMock.getAsync.withArgs(sinon.match(options)).returns(Promise.resolve(body1));//callsArgWith(1, undefined, {statusCode: 200}, JSON.stringify(test_body));
+        requestMock.getAsync.withArgs(sinon.match({url: test_body.paging.next})).returns(Promise.resolve(body2));//callsArgWith(1, undefined, {statusCode: 200}, JSON.stringify(test_body2));
         //requestMock.withArgs(test_body2.paging.next).callsArgWith(1, undefined, {statusCode: 200}, JSON.stringify(test_body3));
         //chart = new Chart(10, 31);
         // chart.on('change', function (body) {
