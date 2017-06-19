c6d7561ed24faf6cc6226cd35f886ca84e87e338
Draco Mithril <dracomithril@gmail.com>
2017-06-20 00:47:15 +0200

    promise all added search for tracks

--- test/components/PlaylistCombiner.test.js
+++ test/components/PlaylistCombiner.test.js
@@ -7,13 +7,14 @@ import PlaylistCombiner from "../../src/front/components/PlaylistCombiner";
 import {shallow} from "enzyme";
 import {shallowToJson} from "enzyme-to-json";
 import configureMockStore from "redux-mock-store";
+const sinon = require('sinon');
 jest.mock('./../../src/front/spotify_utils');
 const initial_state = require('../data/initial_state.json');
 const mockStore = configureMockStore([]);
 describe('<PlaylistCombiner/>', () => {
     it('renders without crashing ChartPresenter', () => {
-        let sp = require('./../../src/front/spotify_utils');
-
+        // let sp = require('./../../src/front/spotify_utils');
+        // let stub = sinon.stub();
         const store = mockStore(initial_state);
         const wrapper = shallow(
             <PlaylistCombiner view_chart={[]}/>, {
