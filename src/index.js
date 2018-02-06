de1bb814e6ca3ef9f31f76a2ea683791b5ef8271
Draco Mithril <dracomithril@gmail.com>
2018-02-06 07:36:03 +0100

    router changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Combiner from "./components/PlaylistCombiner"
 import {routerReducer, syncHistoryWithStore} from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
 import Header from "./components/Header";
@@ -9,9 +10,26 @@ import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import "./index.css";
 import reducers from "./reducers/reducers";
-const {Route, Router} = require('react-router');
+import PropTypes from "prop-types";
 
+const {Route, Router, Redirect, Switch} = require('react-router');
 
+const isAuthenticated = true;
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route {...rest} render={props => (
+        isAuthenticated ? (
+            <Component {...props}/>
+        ) : (
+            <Redirect to={{
+                pathname: '/login',
+                state: {from: props.location}
+            }}/>
+        )
+    )}/>
+);
+PrivateRoute.contextTypes = {
+    store: PropTypes.object
+};
 const store = createStore(
     combineReducers({
         ...reducers,
@@ -27,10 +45,20 @@ ReactDOM.render(
             <div>
                 <Header/>
                 <ErrorConsole/>
-                <Route exact path="/" component={App}/>
+                <Switch>
+                    <Route path="/chart" component={App}/>
+                    {/*<Route path="/login" component={LoginAlert}/>*/}
+                    {/*<PrivateRoute path="/protected" component={App}/>*/}
+                    <Route path={"/combiner"} exact component={Combiner}/>
+                    <Route path={"/api/spotify/callback"} component={(props)=>{
+                        console.log(props);
+                    }} />
+                    <Redirect to="/chart"/>
+
+                </Switch>
             </div>
         </Router>
     </Provider>
-    ,
-    document.getElementById('root')
+,
+document.getElementById('root')
 );
