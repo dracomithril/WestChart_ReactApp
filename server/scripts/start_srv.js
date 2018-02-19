65107088c35274f0c0cde298d9f8ff7c812b5dad
Michal Grezel <michal.grezel@grandparade.co.uk>
2018-02-19 17:16:29 +0100

    implementing submodules

--- server/scripts/start_srv.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Gryzli on 01.04.2017.
- */
-
-process.env.NODE_ENV='development';
-require('dotenv').config({silent: true});
-require('../src/index');
--- web/scripts/start_prod.js
+++
@@ -1,4 +0,0 @@
-process.env.NODE_ENV='production';
-process.env.PORT='3000';
-require('dotenv').config({silent: true});
-require('../../server/src/index');
